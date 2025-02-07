1402c9600f16f4005880cf0ba4df4e802300aaaf
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-02-07 20:40:20 +0900

    usersテーブルに初期データを挿入

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+  )
+end

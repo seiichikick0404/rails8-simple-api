dd23587fae05b76c35ecff2d3617dccdcc3b5842
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-02-09 18:09:19 +0900

    tasksテーブルを追加

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_07_113246) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_09_090750) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

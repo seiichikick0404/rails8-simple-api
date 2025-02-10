2ae643106a9c681ec1cf3b505f733639edcdc2c3
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-02-10 22:12:09 +0900

    tokenの検証処理を追加

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    def authenticate
+        token = request.headers['Authorization'].split(' ').last
+        begin
+            decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })
+            @current_user = User.find(decoded_token[0]['user_id'])
+        rescue JWT::DecodeError => e
+            render json: { error: 'Invalid token' }, status: :unauthorized
+        end
+    end
 end
--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :authenticate, only: [:index, :show, :create, :update, :destroy]
     def index
         @users = User.all
         render json: @users

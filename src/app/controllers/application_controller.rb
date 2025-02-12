455486a91e24285278e59a387e2c54af5d6659d9
seiichi0404 <yamayamaseisei1834@gmail.com>
2025-02-12 21:16:16 +0900

    有効期限を付与

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::API
             @current_user = User.find(decoded_token[0]['user_id'])
         rescue JWT::DecodeError => e
             render json: { error: 'Invalid token' }, status: :unauthorized
+        rescue JWT::ExpiredSignature
+            render json: { error: 'Token expired' }, status: :unauthorized
         end
     end
 end
--- src/app/controllers/authentication_controller.rb
+++ src/app/controllers/authentication_controller.rb
@@ -2,8 +2,10 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by(email: params[:user][:email])
         if @user && @user.authenticate(params[:user][:password])
-            token = JWT.encode({ user_id: @user.id }, Rails.application.credentials.secret_key_base, 'HS256')
-            render json: { user: { id: @user.id, email: @user.email, token: token }}, status: :ok
+            payload = { user_id: @user.id, exp: (DateTime.current + 1.minutes).to_i }
+            secret_key = Rails.application.credentials.secret_key_base
+            token = JWT.encode(payload, secret_key, 'HS256')
+            render json: {user: {email: @user.email, token: token, username: @user.name}}, status: :ok
         else
             render json: { message: "Invalid credentials" }, status: :unauthorized
         end
